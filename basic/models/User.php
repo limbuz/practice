97c5259c8709746ad55e3a30ae6ebe320f1f5955
limbuz <wwlimbuz@gmail.com>
2022-05-27 01:03:46 +0400

    auth settings

--- basic/models/User.php
+++ basic/models/User.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\debug\models\search\Db;
 
 /**
  * This is the model class for table "tbl_user".
@@ -16,8 +17,73 @@ use Yii;
  *
  * @property Post[] $posts
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
+    public $authKey;
+    public $accessToken;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return User::findOne(['username' => $username]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -63,4 +129,6 @@ class User extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Post::className(), ['author_id' => 'id']);
     }
+
+
 }
